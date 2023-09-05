c03cc8f837e5480e7460f266de40dd4a2ebcc9d8
luis <luiszkm@gmail.com>
2023-09-05 09:23:50 -0300

    feat: implements product interface and tests

---
+++ internal/infra/database/product_db_test.go
@@ -0,0 +1,112 @@
+package database
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/luiszkm/api/internal/Domain/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("product", 10.0)
+	assert.NoError(t, err)
+	productDB := NewProduct(db)
+	err = productDB.Create(product)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, product.ID)
+}
+
+func TestFindProductByID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("product", 10.0)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	productFound, err := productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, product.ID, productFound.ID)
+	assert.Equal(t, product.Name, productFound.Name)
+}
+
+func TestFindAllProducts(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+	for i := 1; i < 24; i++ {
+		product, err := entity.NewProduct(fmt.Sprintf("product %d", i), rand.Float64()*100)
+		assert.NoError(t, err)
+		db.Create(product)
+
+	}
+	productDB := NewProduct(db)
+	products, err := productDB.FindAll(1, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+
+	assert.Equal(t, "product 1", products[0].Name)
+	assert.Equal(t, "product 10", products[9].Name)
+
+	products, err = productDB.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "product 11", products[0].Name)
+	assert.Equal(t, "product 20", products[9].Name)
+
+	products, err = productDB.FindAll(3, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 3)
+	assert.Equal(t, "product 21", products[0].Name)
+}
+
+func TestUpdateProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("product", 10.0)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+	product.Name = "product updated"
+	err = productDB.Update(product)
+	assert.NoError(t, err)
+	product, err = productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, product.Name, "product updated")
+}
+
+func TestDeleteProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("product", 10.0)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	err = productDB.Delete(product.ID.String())
+	assert.NoError(t, err)
+
+	_, err = productDB.FindByID(product.ID.String())
+	 assert.Error(t, err)
+}
--- internal/infra/database/user_test.go
+++ internal/infra/database/user_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestCreateUser(t *testing.T) {
-	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,6 +22,25 @@ func TestCreateUser(t *testing.T) {
 	assert.Nil(t, err)
 	var userFound entity.User
 	err = db.First(&userFound, "id =?", user.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("john", "doe@emal.com", "123456")
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
 	assert.Nil(t, err)
 	assert.Equal(t, user.ID, userFound.ID)
 	assert.Equal(t, user.Name, userFound.Name)
